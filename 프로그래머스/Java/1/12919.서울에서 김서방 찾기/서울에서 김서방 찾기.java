6131a3cebb87adfebd2129287e4169e7502ed6b4
ztoacej <ztoacej@gmail.com>
2026-07-02 08:59:57 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.91 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/Java/1/12919.서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] seoul) {
+        int index = Arrays.asList(seoul).indexOf("Kim");
+        return "김서방은 " + index + "에 있다";
+    }
+}
