961763da9f0563e967a2082ec4c980e4bb8a2938
ztoacej <ztoacej@gmail.com>
2026-07-08 09:38:32 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/Java/1/12903.가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String s) {
+        int length = s.length();
+
+        if (length % 2 == 0) {
+            return s.substring(length / 2 - 1, length / 2 + 1);
+        } else {
+            return s.substring(length / 2, length / 2 + 1);
+        }
+    }
+}
