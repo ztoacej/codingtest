2b7d43c01251929b01dfe3491e6e307aee29e772
ztoacej <ztoacej@gmail.com>
2026-07-14 09:39:52 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/Java/1/81301.숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        String[] words = {
+            "zero", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"
+        };
+
+        for (int i = 0; i < words.length; i++) {
+            s = s.replace(words[i], String.valueOf(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
