cb8016ca56c0c16220256953c30ed03882c1b76e
ztoacej <ztoacej@gmail.com>
2026-07-14 09:41:52 +0900

    [level 1] Title: K번째수, Time: 2.05 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/Java/1/42748.K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int k = commands[i][2] - 1;
+
+            int[] sliced = Arrays.copyOfRange(array, start, end);
+
+            Arrays.sort(sliced);
+
+            answer[i] = sliced[k];
+        }
+
+        return answer;
+    }
+}
