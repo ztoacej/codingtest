7b30c2184f823e2f0cbee285b24c65e33b0bddda
ztoacej <ztoacej@gmail.com>
2026-07-21 10:10:56 +0900

    [level 1] Title: 소수 만들기, Time: 1.75 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/Java/1/12977.소수 만들기/소수 만들기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
