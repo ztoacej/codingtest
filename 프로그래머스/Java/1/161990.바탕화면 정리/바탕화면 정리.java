f844c527178b66ebd08122b4b6c28ca9ac1df589
ztoacej <ztoacej@gmail.com>
2026-07-27 05:58:25 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.15 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/Java/1/161990.바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int lux = wallpaper.length;
+        int luy = wallpaper[0].length();
+        int rdx = 0;
+        int rdy = 0;
+
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+
+                    rdx = Math.max(rdx, i + 1);
+                    rdy = Math.max(rdy, j + 1);
+                }
+            }
+        }
+
+        return new int[]{lux, luy, rdx, rdy};
+    }
+}
