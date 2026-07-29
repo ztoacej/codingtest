1cdad99414df5e53cf5cc2403e627afd27d8c581
ztoacej <ztoacej@gmail.com>
2026-07-29 09:26:51 +0900

    [level 2] Title: 피보나치 수, Time: 1.98 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/Java/2/12945.피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int[] fibonacci = new int[n + 1];
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibonacci[i] =
+                    (fibonacci[i - 1] + fibonacci[i - 2]) % 1234567;
+        }
+
+        return fibonacci[n];
+    }
+}
