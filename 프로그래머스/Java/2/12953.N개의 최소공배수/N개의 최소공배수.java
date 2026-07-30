80702a043ce4de7fb7cb076e6d0b269c96532f81
ztoacej <ztoacej@gmail.com>
2026-07-30 09:31:07 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/Java/2/12953.N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            answer = lcm(answer, arr[i]);
+        }
+
+        return answer;
+    }
+
+    private int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+
+        return a;
+    }
+}
