23f3b7ce487c044a2840d2e1cc15261667646b6a
ztoacej <ztoacej@gmail.com>
2026-08-21 10:03:08 +0900

    [level 2] Title: 큰 수 만들기, Time: 17.07 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/Java/2/42883.큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder stack = new StringBuilder();
+
+        for (int i = 0; i < number.length(); i++) {
+            char current = number.charAt(i);
+
+            while (k > 0
+                    && stack.length() > 0
+                    && stack.charAt(stack.length() - 1) < current) {
+
+                stack.deleteCharAt(stack.length() - 1);
+                k--;
+            }
+
+            stack.append(current);
+        }
+
+        if (k > 0) {
+            stack.setLength(stack.length() - k);
+        }
+
+        return stack.toString();
+    }
+}
